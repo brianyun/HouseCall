bca3fe5fdf7a6b4c18abcaace773b1732d2fe0e0
bcb225 <bcb225@gmail.com>
2018-11-28 16:03:33 +0900

    restAPI for getRating

--- restAPI/argv.js
+++
@@ -1,9 +0,0 @@
-const yargs = require('yargs');
-
-module.exports = yargs
-  .option('user', {
-    default: '',
-  })
-  .option('password', {
-    default: '',
-  }).argv;
--- restAPI/models/index.js
+++
@@ -1,2 +0,0 @@
-// TODO(dykim): Depricated. mysql을 바로 쓰는 방향으로 refactoring 해야함.
-require('./user');
