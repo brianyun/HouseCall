51f4ecb22c317998e89c0636adc8c8a0419b5c2b
윤희상 <huisang.growth@gmail.com>
2018-12-11 12:58:55 +0900

    Vuetify setup

---
+++ frontend/src/components/Landing.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="white elevation-2">
+    <v-toolbar flat dense class="cyan" dark>
+      <v-toolbar-title dark>Title</v-toolbar-title>
+
+      <slot name="action"></slot>
+    </v-toolbar>
+
+    <div class="pl-4 pr-4 pt-3 pb-3">
+      <slot>No slot content defined.</slot>
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  async mounted() {
+    console.log("Landing.vue mounted()");
+  }
+};
+</script>
+
+<style scoped>
+</style>
