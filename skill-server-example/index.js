31bdce0fb38eb3f7f0c3b920ecb6f5d8e1ec5631
bcb225 <bcb225@gmail.com>
2018-12-15 15:38:31 +0900

    .

--- skill-server-example/index.js
+++ skill-server-example/index.js
@@ -52,41 +52,41 @@ apiRouter.post('/showHello', function(req, res) {
 apiRouter.post('/commerceCardTest', function(req, res){
     console.log(req.body);
     const responseBody = {
-        "version": "2.0",
-        "template": {
-          "outputs": [
+        version: "2.0",
+        template: {
+          outputs: [
             {
-              "commerceCard": {
-                "description": "따끈따끈한 보물 상자 팝니다",
-                "price": 10000,
-                "discount": 1000,
-                "currency": "won",
-                "thumbnails": [
+              commerceCard: {
+                description: "따끈따끈한 보물 상자 팝니다",
+                price: 10000,
+                discount: 1000,
+                currency: "won",
+                thumbnails: [
                   {
-                    "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg",
-                    "link": {
-                      "web": "https://store.kakaofriends.com/kr/products/1542"
+                    imageUrl: "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg",
+                    link: {
+                      web: "https://store.kakaofriends.com/kr/products/1542"
                     }
                   }
                 ],
-                "profile": {
-                  "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4BJ9LU4Ikr_EvZLmijfcjzQKMRCJ2bO3A8SVKNuQ78zu2KOqM",
-                  "nickname": "보물상자 팝니다"
+                profile: {
+                  imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4BJ9LU4Ikr_EvZLmijfcjzQKMRCJ2bO3A8SVKNuQ78zu2KOqM",
+                  nickname: "보물상자 팝니다"
                 },
-                "buttons": [
+                buttons: [
                   {
-                    "label": "구매하기",
-                    "action": "webLink",
-                    "webLinkUrl": "https://store.kakaofriends.com/kr/products/1542"
+                    label: "구매하기",
+                    action: "webLink",
+                    webLinkUrl: "https://store.kakaofriends.com/kr/products/1542"
                   },
                   {
-                    "label": "전화하기",
-                    "action": "phone",
-                    "phoneNumber": "354-86-00070"
+                    label: "전화하기",
+                    action: "phone",
+                    phoneNumber: "354-86-00070"
                   },
                   {
-                    "label": "공유하기",
-                    "action": "share"
+                    label: "공유하기",
+                    action: "share"
                   }
                 ]
               }
